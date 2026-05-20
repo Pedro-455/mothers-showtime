1a8a51a1916205a50381fb13f64f254347869e28
Pedro-455 <pe@mothers.co.nz>
2026-05-20 21:54:01 +1200

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register.jsx";
+import Admin from "./pages/Admin.jsx";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Entrant registration page */}
+        <Route path="/b7k9f2-register" element={<Register />} />
+
+        {/* Admin page */}
+        <Route path="/admin" element={<Admin />} />
+
+        {/* Fallback for unknown routes */}
+        <Route
+          path="*"
+          element={
+            <div style={{ padding: 40, fontSize: 20 }}>
+              Page not found.
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
