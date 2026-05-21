5bb720967c6c5630e2763add89a7ea4a625a1ba9
Pedro-455 <pe@mothers.co.nz>
2026-05-21 17:02:02 +1200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Register from "./pages/b7k9f2-register.jsx";
+import Admin from "./pages/Admin.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const path = window.location.pathname;
+
+let Page = App;
+
+if (path === "/b7k9f2-register") Page = Register;
+if (path === "/admin") Page = Admin;
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Page />);
