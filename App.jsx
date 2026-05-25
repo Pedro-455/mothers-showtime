103495f75934ba6736a6036215097c9251fba89c
Pedro-455 <pe@mothers.co.nz>
2026-05-25 18:11:33 +1200

    Remove Admin route from App component
    Removed Admin route and related comments from App component.

--- App.jsx
+++ App.jsx
@@ -1,16 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/b7k9f2-register.jsx";
-import Admin from "./pages/Admin.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Entrant registration page */}
         <Route path="/b7k9f2-register" element={<Register />} />
-        {/* Admin page */}
-        <Route path="/admin" element={<Admin />} />
-        {/* Fallback for unknown routes */}
         <Route
           path="*"
           element={
