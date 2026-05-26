1f4de10f6b8da7a9649396c74023700ae083cbb7
Pedro-455 <pe@mothers.co.nz>
2026-05-26 17:22:29 +1200

    Add CorvetteDemo route to App component

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/b7k9f2-register.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
+import CorvetteDemo from "./pages/CorvettDemo.jsx";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/b7k9f2-register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/corvette-z06" element={<CorvetteDemo />} />
         <Route
           path="*"
           element={
