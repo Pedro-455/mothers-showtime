9a377c25aba77fc43bb9189edca5c93365c6bd0a
Pedro-455 <pe@mothers.co.nz>
2026-05-28 18:44:07 +1200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -6,6 +6,7 @@ import CadillacDemo from "./pages/CadillacDemo.jsx";
 import YukonDemo from "./pages/YukonDemo.jsx";
 import HarleyDemo from "./pages/HarleyDemo.jsx";
 import AstonDBX707 from "./pages/AstonDBX707.jsx";
+import AstonDB12S from "./pages/AstonDB12S.jsx";
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
         <Route path="/gmc-yukon" element={<YukonDemo />} />
         <Route path="/harley-street-glide" element={<HarleyDemo />} />
         <Route path="/aston-dbx707" element={<AstonDBX707 />} />
+        <Route path="/aston-db12s" element={<AstonDB12S />} />
         <Route
           path="*"
           element={
